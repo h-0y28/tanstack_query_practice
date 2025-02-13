c33b6a325e00205e239e25dd33a8daf87d0c0df3
h-0y28 <s24015@gsm.hs.kr>
2025-02-14 02:38:29 +0900

    feat: add feature to fetch and display posts from API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,47 @@
-export default function Home() {
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+const Page = () => {
+  const [posts, setPosts] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError("데이터 불러오기 실패");
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>로딩 중..</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>전체 게시글 목록</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+export default Page;
